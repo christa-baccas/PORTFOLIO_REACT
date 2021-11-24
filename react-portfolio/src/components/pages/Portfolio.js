a1f58bc3916676e6c5bb252426d3d25c324b400a
christa-baccas <christa.baccas@gmail.com>
2021-11-24 16:48:28 -0600

    updated title on portfolio images

--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -5,7 +5,7 @@ import Img3 from "../../../src/images/sweepers.png";
 import Img4 from "../../../src/images/budget.png";
 import Img5 from "../../../src/images/Weather.png";
 import Img6 from "../../../src/images/Password.png";
-import Img7 from "../../../src/images/note.png";
+import Img7 from "../../../src/images/NoteTaker.png";
 import "../styles/Portfolio.css";
 
 export default function Portfolio() {
@@ -14,45 +14,39 @@ export default function Portfolio() {
       <Card style={{ width: "30rem" }} className='card'>
         <Card.Img variant="top" src={Img2} className='img'/>
         <Card.Body>
-          <Card.Title>FanJam Event and Lyrics</Card.Title>
           <a href="https://bradmw.github.io/FanJam-Project-1/">
-            Open Application Here!
+          FanJam Events and Lyrics
           </a>
         </Card.Body>
       </Card>
       <Card style={{ width: "30rem" }} className='card'>
         <Card.Img variant="top" src={Img3}className='img' />
         <Card.Body>
-          <Card.Title>Sweeper's Chores</Card.Title>
-          <a href="https://sweeper-chore-chart.herokuapp.com/">Open Application Here!</a>
+          <a href="https://sweeper-chore-chart.herokuapp.com/">Sweeper's Chores</a>
         </Card.Body>
       </Card>
       <Card style={{ width: "30rem" }} className='card'>
         <Card.Img variant="top" src={Img4}className='img' />
         <Card.Body>
-          <Card.Title>Budget Tracker</Card.Title>
-          <a href="https://cab-budget-tracker.herokuapp.com/">Open Application Here!</a>
+          <a href="https://cab-budget-tracker.herokuapp.com/">Budget Tracker</a>
         </Card.Body>
       </Card>
       <Card style={{ width: "30rem" }} className='card'>
         <Card.Img variant="top" src={Img5}className='img' />
         <Card.Body>
-          <Card.Title>Weather Dashboard</Card.Title>
-          <a href="https://christa-baccas.github.io/WEATHER_DASHBOARD/">Open Application Here!</a>
+          <a href="https://christa-baccas.github.io/WEATHER_DASHBOARD/">Weather Dashboard</a>
         </Card.Body>
       </Card>
       <Card style={{ width: "30rem" }} className='card'>
         <Card.Img variant="top" src={Img6}className='img' />
         <Card.Body>
-          <Card.Title>Password Generator</Card.Title>
-          <a href="https://christa-baccas.github.io/PASSWORD_GENERATOR/">Open Application Here!</a>
+          <a href="https://christa-baccas.github.io/PASSWORD_GENERATOR/">Password Generator</a>
         </Card.Body>
       </Card>
       <Card style={{ width: "30rem" }} className='card'>
         <Card.Img variant="top" src={Img7}className='img' />
         <Card.Body>
-          <Card.Title>Note Taker</Card.Title>
-          <a href="https://homework-11-note-taker-app-cab.herokuapp.com/">Open Application Here!</a>
+          <a href="https://homework-11-note-taker-app-cab.herokuapp.com/">Note Taker</a>
         </Card.Body>
       </Card>
     </div>
